5522abc10f6cceede418989007350d69d2b754c3
b-cb <batiste.cometbarthe@etu.inp-n7.fr>
2026-04-01 14:13:32 +0200

    init

---
+++ backend-cartes/src/test/java/fr/enseeiht/jeux/backend_cartes/BackendCartesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enseeiht.jeux.backend_cartes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCartesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
